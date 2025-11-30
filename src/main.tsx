f6ec438714ddf032e3e3b76e984ee5c95e8bab42
arefks <arefks@gmail.com>
2025-11-30 09:53:19 +0100

    added stuff for pages
    added stuff for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>ReportPilot (Dev Build)</h1>
+      <p>This is the Vite dev/production React entry point.</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
